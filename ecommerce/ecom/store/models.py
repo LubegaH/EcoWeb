a200e5f51fc27a43e4d6bfb7271d198968a314eb
Ham <lubegah@gmail.com>
2020-05-27 15:50:21 +0300

    Handle errors if image not present

--- ecommerce/ecom/store/models.py
+++ ecommerce/ecom/store/models.py
@@ -18,6 +18,15 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+    # Prevents errors incase no image is loaded
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url = ''
+        return url
 
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
